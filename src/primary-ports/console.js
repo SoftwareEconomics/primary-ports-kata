3d670a12ce2d3cc03ab57ae4d5fc35a475b5cf9b
Guillermo Gutiérrez <ggalmazor@gmail.com>
2019-05-31 10:55:06 +0200

    Replace readline-sync npm module with inquirer for richer CLI interaction

--- src/primary-ports/console.js
+++ src/primary-ports/console.js
@@ -1,12 +1,14 @@
 const PrimaryPort = require('./primary-port');
-const readlineSync = require('readline-sync');
+const inquirer = require("inquirer");
 
-module.exports = new PrimaryPort(callback => {
+module.exports = new PrimaryPort(async callback => {
   while (true) {
-    const vertical = readlineSync.question('Vertical: ');
-    const feature = readlineSync.question('Feature: ');
-    const data = JSON.parse(readlineSync.question('Data: '));
-    const result = callback(vertical, feature, data);
+    const {vertical, feature, data} = await inquirer.prompt([
+      {name: "vertical", message: "Vertical name"},
+      {name: "feature", message: "Feature name"},
+      {name: "data", message: "Data"}
+    ]);
+    const result = callback(vertical, feature, JSON.parse(data));
     console.log(result);
   }
 });
